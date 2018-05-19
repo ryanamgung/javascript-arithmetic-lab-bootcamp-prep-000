034e8320524f2569000195e54ea587f03485b2ea
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-19 20:47:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,3 +23,19 @@ function inc(n)
   n++
   return n
 }
+
+function dec(n)
+{
+  n--
+  return n
+}
+
+function makeInt(n)
+{
+  return parseInt(n)
+}
+
+function preserveDecimal(n)
+{
+  return parseFloat(n)
+}
